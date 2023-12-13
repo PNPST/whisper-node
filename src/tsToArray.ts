3012408b8e2c8c57f28e6b42e6b9209f7dbce6e7
Steven <steven@pineapplestudio.com.au>
2023-12-13 13:55:59 +1100

    Remove lines.shift

--- src/tsToArray.ts
+++ src/tsToArray.ts
@@ -7,11 +7,7 @@ export type ITranscriptLine = {
 export default function parseTranscript(vtt: string): ITranscriptLine[] {
   // 1. separate lines by matching the format like "[00:03:04.000 --> 00:03:13.000]   XXXXXX"
   const lines: string[] = vtt.match(/\[[0-9:.]+\s-->\s[0-9:.]+\].*/g);
-
-  // 2. remove the first line, which is empty
-  lines.shift();
-
-  // 3. convert each line into an object
+  // 2. convert each line into an object
   return lines.map(line => {
     // 3a. split ts from speech
     let [timestamp, speech] = line.split(']  '); // two spaces (3 spaces doesn't work with punctuation like period . )
